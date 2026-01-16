a1e7a771b8cbb1ecb134f71c7a9a09ed88bcfc6d
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-16 17:25:41 +0000

    Testing Fri Jan 16 2026 17:25:41 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex06/student/SongLyrics.java
+++ chapter1/ex06/student/SongLyrics.java
@@ -1,1 +1,9 @@
 // Write your code here
+class SongLyrics{
+    public static void main(String[] args) {
+        System.out.println("Somewhere over the rainbow");
+        System.out.println("Way up high");
+        System.out.println("There's a land that I heard of");
+        System.out.println("Once in a lullaby");
+    }
+}
