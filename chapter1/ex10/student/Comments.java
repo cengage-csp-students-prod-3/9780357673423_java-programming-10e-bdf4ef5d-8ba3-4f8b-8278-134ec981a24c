88ff7b69175a8c5ea6246d8362faf1aafbfaf493
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-21 17:38:42 +0000

    Testing Wed Jan 21 2026 17:38:42 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex10/student/Comments.java
+++ chapter1/ex10/student/Comments.java
@@ -1,1 +1,18 @@
 // Write your code here
+class Comments {
+    public static void main(String[] args) {
+        System.out.println("Program comments are nonexecuting statements ");
+        System.out.println("you add to a file for documentation.");
+
+        //Comments methods
+        //-Line comments
+            //Program comments are nonexecuting statements
+            //you add to a file for documentation.
+        /*-Block comments
+            Program comments are nonexecuting statements
+            you add to a file for documentation.*/
+        /**-Javadoc comments
+             Program comments are nonexecuting statements
+            you add to a file for documentation.*/
+    }
+}
