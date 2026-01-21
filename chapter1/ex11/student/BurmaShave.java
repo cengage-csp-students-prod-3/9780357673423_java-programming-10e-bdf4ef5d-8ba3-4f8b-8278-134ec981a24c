978d8aa6bd657b4229576bc4b87a936ad870b0b5
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-21 18:05:15 +0000

    Testing Wed Jan 21 2026 18:05:15 GMT+0000 (Coordinated Universal Time)

--- chapter1/ex11/student/BurmaShave.java
+++ chapter1/ex11/student/BurmaShave.java
@@ -1,1 +1,10 @@
 // Write your code here
+class BurmaShave {
+    public static void main(String[] args) {
+        System.out.println("I proposed to Ida");
+        System.out.println("Ida refused");
+        System.out.println("Ida won my Ida");
+        System.out.println("If Ida used");
+        System.out.println("Burma-Shave.");
+    }
+}
