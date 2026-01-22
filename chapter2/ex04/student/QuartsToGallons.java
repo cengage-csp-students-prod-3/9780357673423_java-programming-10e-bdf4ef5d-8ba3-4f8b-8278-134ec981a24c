ebaacd368950fb034e6bd89c9f1623a42cf22151
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-22 13:14:14 +0000

    Testing Thu Jan 22 2026 13:14:14 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex04/student/QuartsToGallons.java
+++ chapter2/ex04/student/QuartsToGallons.java
@@ -1,1 +1,8 @@
-// Write your code here
+public class QuartsToGallons {
+    public static void main(String[] args) {
+        final int QUARTS_IN_GALLON = 4;
+        int quartsNeeded = 18;
+        System.out.println("A job that needs "+quartsNeeded+ " quarts requires "+QUARTS_IN_GALLON+" gallons plus 2 quarts.");
+    }
+    
+}
