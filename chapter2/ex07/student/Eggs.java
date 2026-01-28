5143d65701b084af219c17aa8c916cee42af69a9
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-28 20:27:33 +0000

    Testing Wed Jan 28 2026 20:27:33 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex07/student/Eggs.java
+++ chapter2/ex07/student/Eggs.java
@@ -1,1 +1,37 @@
 // Write your code here
+import java.util.Scanner;
+/*
+ VARIABLES
+final double dozenEggPrice = 3.25
+final double singleEggPrice = 0.45
+int quantityEggs 
+int dozen = 12
+int totaloutOfDozen = qE % dZ
+int totalDozen = quantityEggs / dozen
+int totalDozenPrice = totalDozen * dozenEggPrice
+int totaloutOfDozenPrice = outOfDozen * singleEggPrice
+int totalPrice = totalDozenPrice + outOfDozenPrice
+
+INPUT << number of eggs in the order
+
+OUTPUT >> 
+You ordered +quantityEggs+ eggs. That's +totalDozen+ dozen at +dozenEggPrice+ per dozen and +outOfDozen+ loose eggs 
+at singleEggPrice cents each for a total of  
+*/
+public class Eggs {
+    public static void main(String[] args) {
+        final double dozenEggPrice = 3.25;
+        final double singleEggPrice = 0.45;
+        final int dozen = 12;
+        int quantityEggs;
+        Scanner inputEggs = new Scanner(System.in);
+        System.out.print("Enter the quantity of eggs order << ");
+        quantityEggs = inputEggs.nextInt();
+        int totalDozen = quantityEggs / dozen;
+        int totaloutOfDozen = quantityEggs % dozen;
+        double totalDozenPrice = totalDozen * dozenEggPrice;
+        double totaloutOfDozenPrice = totaloutOfDozen * singleEggPrice;
+        double totalPrice = totalDozenPrice + totaloutOfDozenPrice;
+        System.out.println("You ordered "+quantityEggs+" eggs. That's "+totalDozen+" dozen at $"+dozenEggPrice+ " per dozen and "+totaloutOfDozen+" loose eggs at $"+singleEggPrice+" cents each for a total of $"+totalPrice);
+    }
+}
