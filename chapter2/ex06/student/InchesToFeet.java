5ad77135e15159c6bfd93264b39dfe9cfeb068a6
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-28 16:52:01 +0000

    Testing Wed Jan 28 2026 16:52:01 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex06/student/InchesToFeet.java
+++ chapter2/ex06/student/InchesToFeet.java
@@ -1,1 +1,7 @@
 // Write your code here
+public class InchesToFeet {
+    public static void main(String[] args) {
+        int inches = 86;
+        System.out.println(inches+" inches is 7 feet and 2 inches");
+    }
+}
