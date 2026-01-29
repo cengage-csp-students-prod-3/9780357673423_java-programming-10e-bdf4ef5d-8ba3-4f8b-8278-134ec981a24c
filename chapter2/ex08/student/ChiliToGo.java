9565840aa1acc8cc7faa0ead0e27e9bdd3158067
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-01-29 16:01:46 +0000

    Testing Thu Jan 29 2026 16:01:46 GMT+0000 (Coordinated Universal Time)

--- chapter2/ex08/student/ChiliToGo.java
+++ chapter2/ex08/student/ChiliToGo.java
@@ -1,1 +1,38 @@
 // Write your code here
+/*
+double mealAdultPrice = 7
+double mealChildPrice = 4
+int adultMeals 
+int childMeals
+double totalAdultMeals = mealAdultPrice * adultMeals
+double totalChildMeals = mealChildPrice * childMeals
+double totalOrder = totalAdultMeals + totalChildMeals
+*/
+
+import java.util.Scanner;
+
+public class ChiliToGo {
+
+    public static void main(String[] args) {
+        final double mealAdultPrice = 7;
+        final double mealChildPrice = 4;
+        int adultMeals;
+        int childMeals;
+        double totalAdultMeals;
+        double totalChildMeals;
+        double totalOrder;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter number of adult meals ordered >>");
+        adultMeals = keyboard.nextInt();
+        System.out.print("Enter number of child meals ordered >>");
+        childMeals = keyboard.nextInt();
+        totalAdultMeals = mealAdultPrice * adultMeals;
+        totalChildMeals = mealChildPrice * childMeals;
+        totalOrder = totalAdultMeals + totalChildMeals;
+        System.out.println(adultMeals+" adult meals were ordered at "+mealAdultPrice+ "each");
+        System.out.println("        Total is "+totalAdultMeals);
+        System.out.println(childMeals+" adult meals were ordered at "+mealChildPrice+ "each");
+        System.out.println("        Total is "+totalChildMeals);
+        System.out.println("Grand total for all meals is $"+totalOrder);
+    }
+}
--- chapter2/ex08/student/ChiliToGoProfit.java
+++ chapter2/ex08/student/ChiliToGoProfit.java
@@ -1,1 +1,50 @@
 // Write your code here
+/*
+double mealAdultProduce
+double mealChildProduce
+int adultMeals
+int childMeals
+double adultProfitOrder = (mealAdultPrice - mealAdultProduce) *adultMeals
+double childProfitOrder = (mealChildPrice - mealChildProduce) *childMeals
+double totalProfit = adultProfitOrder + childProfitOrder
+*/
+import java.util.Scanner;
+
+public class ChiliToGoProfit {
+
+    public static void main(String[] args) {
+        final double mealAdultPrice = 7;
+        final double mealChildPrice = 4;
+        final double mealAdultProduce = 4.35;
+        final double mealChildProduce = 3.10;
+        int adultMeals;
+        int childMeals;
+        double totalAdultMeals;
+        double totalChildMeals;
+        double totalOrder;
+        double adultProfitOrder;
+        double childProfitOrder;
+        double totalProfit;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter number of adult meals ordered >>");
+        adultMeals = keyboard.nextInt();
+        System.out.print("Enter number of child meals ordered >>");
+        childMeals = keyboard.nextInt();
+        totalAdultMeals = mealAdultPrice * adultMeals;
+        totalChildMeals = mealChildPrice * childMeals;
+        totalOrder = totalAdultMeals + totalChildMeals;
+        System.out.println(adultMeals+" adult meals were ordered at "+mealAdultPrice+ "each");
+        System.out.println("        Total is "+totalAdultMeals);
+        System.out.println(childMeals+" adult meals were ordered at "+mealChildPrice+ "each");
+        System.out.println("        Total is "+totalChildMeals);
+        System.out.println("Grand total for all meals is $"+totalOrder);
+        System.out.println("Profits");
+        adultProfitOrder = (mealAdultPrice - mealAdultProduce) * adultMeals;
+        System.out.println("        Adult profit is $"+adultProfitOrder);
+        childProfitOrder = (mealChildPrice - mealChildProduce) * childMeals;
+        System.out.println("        Child profit is $"+childProfitOrder);
+        totalProfit = adultProfitOrder + childProfitOrder;
+        System.out.println("Total profit is $"+totalProfit);
+    }
+    
+}
