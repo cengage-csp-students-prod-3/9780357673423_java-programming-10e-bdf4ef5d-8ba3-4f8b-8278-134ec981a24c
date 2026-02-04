0e09488fae7b6680710658f3682029a61007f4c8
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-02-04 16:45:10 +0000

    Testing Wed Feb 04 2026 16:45:10 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex01/student/EvenOdd.java
+++ chapter5/ex01/student/EvenOdd.java
@@ -1,1 +1,33 @@
-// Write your code here
+// Write your code here 
+/*
+ANALYSIS
+    given an integer, display a statement that indicates whether the integer is even or odd.
+DESIGN
+    INPUT
+        int getNumber
+    LOGIC
+        if getNumber%2=0 
+            print(getNumber " is even")
+        else
+            print("is odd")
+*/
+
+import java.util.Scanner;
+
+public class EvenOdd {
+
+    public static void main(String[] args) {
+        isEven();
+    }
+    public static int isEven(){
+        int getNumber;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.print("Enter a number >> ");
+        getNumber = keyboard.nextInt();
+        if ((getNumber%2) == 0) 
+            System.out.println(getNumber+" is even.");
+        else
+            System.out.println(getNumber+" is odd.");
+        return getNumber;
+    }
+}
