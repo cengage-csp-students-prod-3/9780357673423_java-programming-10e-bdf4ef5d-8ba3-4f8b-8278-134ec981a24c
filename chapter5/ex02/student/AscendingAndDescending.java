d4190c96c47dd0a63a8a2cb4c3f38ece38be7503
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-02-05 08:27:19 +0000

    Testing Thu Feb 05 2026 08:27:19 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/AscendingAndDescending.java
+++ chapter5/ex02/student/AscendingAndDescending.java
@@ -19,54 +19,44 @@ public class AscendingAndDescending {
         
         //ascending order
         System.out.print("Ascending:");
-        if (n1 >= n2 && n1 >= n3) {
-            if (n2 > n3) {
+        if (n1<=n2 && n1<=n3) {
+            if (n2<=n3) {
                 System.out.println(n1+" "+n2+" "+n3);
-            }
-            else if(n3>n2) {
+            } else {
                 System.out.println(n1+" "+n3+" "+n2);
             }
-        }
-        if (n2 >= n1 && n2 >= n3) {
-            if (n1 > n3) {
+        } else if (n2<=n1 && n2<=n3) {
+            if (n1<=n3) {
                 System.out.println(n2+" "+n1+" "+n3);
-            }
-            else if(n3>n1) {
+            } else {
                 System.out.println(n2+" "+n3+" "+n1);
             }
-        }
-        if (n3 >= n1 && n3 >= n2) {
-            if (n2 > n1) {
+        } else {
+            if (n1<=n2) {
+                System.out.println(n3+" "+n1+" "+n2);                
+            } else {
                 System.out.println(n3+" "+n2+" "+n1);
-            }
-            else if(n1>n2) {
-                System.out.println(n3+" "+n1+" "+n2);
             }
         }
         //descending order
         System.out.print("Descending:");
-        if (n1 < n2 && n1 < n3) {
-            if (n2 < n3) {
-                System.out.println(n3+" "+n2+" "+n1);
-            }
-            else if(n3<n2) {
-                System.out.println(n2+" "+n3+" "+n1);
-            }
-        }
-        if (n2 < n1 && n2 < n3) {
-            if (n1 < n3) {
-                System.out.println(n3+" "+n1+" "+n2);
-            }
-            else if(n3<n1) {
+        if (n1>=n2 && n1>=n3) {
+            if (n2>=n3) {
+                System.out.println(n1+" "+n2+" "+n3);
+            } else {
                 System.out.println(n1+" "+n3+" "+n2);
             }
-        }
-        if (n3 < n1 && n3 < n2) {
-            if (n2 < n1) {
-                System.out.println(n1+" "+n2+" "+n3);
+        } else if (n2>=n1 && n2>=n3) {
+            if (n1>=n3) {
+                System.out.println(n2+" "+n1+" "+n3);
+            } else {
+                System.out.println(n2+" "+n3+" "+n1);
             }
-            else if(n1<n2) {
-                System.out.println(n3+" "+n1+" "+n3);
+        } else {
+            if (n1>=n2) {
+                System.out.println(n3+" "+n1+" "+n2);                
+            } else {
+                System.out.println(n3+" "+n2+" "+n1);
             }
         }
     }
