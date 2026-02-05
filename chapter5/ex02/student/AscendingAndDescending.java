f3d04aeafc8c09e32db40b62e5b55f53486a348f
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-02-05 07:56:56 +0000

    Testing Thu Feb 05 2026 07:56:56 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex02/student/AscendingAndDescending.java
+++ chapter5/ex02/student/AscendingAndDescending.java
@@ -1,1 +1,73 @@
 // Write your code here
+/*
+Write an application called AscendingAndDescending that asks a user to enter three integers. 
+Display them in ascending and descending order.
+*/
+
+import java.util.Scanner;
+
+public class AscendingAndDescending {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int n1, n2, n3;
+        System.out.print("Enter an integer...");
+        n1 = keyboard.nextInt();        
+        System.out.print("And another...");
+        n2 = keyboard.nextInt();
+        System.out.print("And just one more... ");
+        n3 = keyboard.nextInt();
+        
+        //ascending order
+        System.out.print("Ascending:");
+        if (n1 >= n2 && n1 >= n3) {
+            if (n2 > n3) {
+                System.out.println(n1+" "+n2+" "+n3);
+            }
+            else if(n3>n2) {
+                System.out.println(n1+" "+n3+" "+n2);
+            }
+        }
+        if (n2 >= n1 && n2 >= n3) {
+            if (n1 > n3) {
+                System.out.println(n2+" "+n1+" "+n3);
+            }
+            else if(n3>n1) {
+                System.out.println(n2+" "+n3+" "+n1);
+            }
+        }
+        if (n3 >= n1 && n3 >= n2) {
+            if (n2 > n1) {
+                System.out.println(n3+" "+n2+" "+n1);
+            }
+            else if(n1>n2) {
+                System.out.println(n3+" "+n1+" "+n2);
+            }
+        }
+        //descending order
+        System.out.print("Descending:");
+        if (n1 < n2 && n1 < n3) {
+            if (n2 < n3) {
+                System.out.println(n3+" "+n2+" "+n1);
+            }
+            else if(n3<n2) {
+                System.out.println(n2+" "+n3+" "+n1);
+            }
+        }
+        if (n2 < n1 && n2 < n3) {
+            if (n1 < n3) {
+                System.out.println(n3+" "+n1+" "+n2);
+            }
+            else if(n3<n1) {
+                System.out.println(n1+" "+n3+" "+n2);
+            }
+        }
+        if (n3 < n1 && n3 < n2) {
+            if (n2 < n1) {
+                System.out.println(n1+" "+n2+" "+n3);
+            }
+            else if(n1<n2) {
+                System.out.println(n3+" "+n1+" "+n3);
+            }
+        }
+    }
+}
