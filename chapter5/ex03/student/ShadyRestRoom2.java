d57291a3ae3f405ce29788c6bd586e60dd0d8f7f
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-02-11 11:59:37 +0000

    Testing Wed Feb 11 2026 11:59:37 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex03/student/ShadyRestRoom2.java
+++ chapter5/ex03/student/ShadyRestRoom2.java
@@ -31,11 +31,11 @@ public class ShadyRestRoom2 {
         switch (getChoose) {
             case 1:
                 if (getView == 1) {
-                    System.out.println("You selected Queen bed with a lake view $"+140);                
+                    System.out.println("You selected Queen bed with a lake view $"+(140+15));                
                 } else if (getView == 2) {
                     System.out.println("You selected Queen bed with a park view $125");
                 } else {
-                    System.out.println("Invalid entry view $"+140);
+                    System.out.println("Invalid entry view $"+(140+15));
                 }
                 break;
             case 2:
@@ -49,11 +49,11 @@ public class ShadyRestRoom2 {
                 break;
             case 3:
                 if (getView == 1) {
-                    System.out.println("You selected King bed and a pullout couch with a lake view $179");                
+                    System.out.println("You selected King bed and a pullout couch with a lake view $"+(156+15));                
                 } else if (getView == 2) {
-                    System.out.println("You selected King and a  pullout couch bed with a park view $165");
+                    System.out.println("You selected King and a  pullout couch bed with a park view $"+165);
                 } else {
-                    System.out.println("Invalid entry view $179");
+                    System.out.println("Invalid entry view $"+(156+15));
                 }
                 break;
             default:
