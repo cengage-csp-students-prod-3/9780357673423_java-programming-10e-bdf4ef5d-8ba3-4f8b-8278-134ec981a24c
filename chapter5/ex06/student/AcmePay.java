0517299c12c460814aa59c42b59c7f874b359a21
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-02-12 19:03:25 +0000

    Testing Thu Feb 12 2026 19:03:25 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/AcmePay.java
+++ chapter5/ex06/student/AcmePay.java
@@ -48,7 +48,7 @@ public class AcmePay {
         final double FIRST_SHIFT=17, SECOND_SHIFT=18.50, THIRD_SHIFT=22;
         int hoursWorked, getShift, getRPlan;
         final double OT_RATE=1.5, RETIREMENT_DEDUCTION=0.03;
-        double totalRegWeekPay=0, totalOTWeekPay=0, netPay=0, payRate=0, totalRetPlan=0, grossPay=0;
+        double totalRegWeekPay=0, totalOTWeekPay=0, netPay=0, payRate=0, totalDedRetPlan=0;
         final int REG_WEEK_HOURS=40;
 
         Scanner keyboard = new Scanner(System.in);
@@ -76,22 +76,21 @@ public class AcmePay {
             totalOTWeekPay = 0;
         }
         //RETIREMENT PLAN
-        grossPay = totalOTWeekPay + totalRegWeekPay;
         if (getShift == 2 || getShift == 3) {
             System.out.println("Do you want to participate in the retirement plan?");
             System.out.println("   Enter 1 for Yes and 2 for No >> ");
             getRPlan = keyboard.nextInt();
             if (getRPlan == 1) {
-                totalRetPlan = grossPay * RETIREMENT_DEDUCTION;
+                totalDedRetPlan = totalRegWeekPay * RETIREMENT_DEDUCTION;
             } else if (getRPlan == 2) {
-                totalRetPlan = 0;
+                totalDedRetPlan = 0;
             } else {
                 return;
                 
             }
         } 
         //NETPAY
-        netPay = grossPay - totalRetPlan;
+        netPay = totalRegWeekPay + totalOTWeekPay - totalDedRetPlan;
         
         //PRINT
         System.out.println("Hours worked is    "+hoursWorked);
@@ -99,7 +98,7 @@ public class AcmePay {
         System.out.println("Hourly pay rate is  "+payRate);
         System.out.println("Regular pay is     "+totalRegWeekPay);
         System.out.println("Overtime pay is    "+totalOTWeekPay);
-        System.out.println("Retirement deduction is "+totalRetPlan);
+        System.out.println("Retirement deduction is "+totalDedRetPlan);
         System.out.println("     Net pay is.............."+netPay);
     }
     
