ff8098a1ecc219cf82ba7d35865f62163c62816c
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-02-12 19:21:36 +0000

    Testing Thu Feb 12 2026 19:21:36 GMT+0000 (Coordinated Universal Time)

--- chapter5/ex06/student/AcmePay.java
+++ chapter5/ex06/student/AcmePay.java
@@ -49,7 +49,7 @@ public class AcmePay {
         int hoursWorked, getShift, getRPlan;
         final double OT_RATE=1.5, RETIREMENT_DEDUCTION=0.03;
         double totalRegWeekPay=0, totalOTWeekPay=0, netPay=0, payRate=0, totalDedRetPlan=0;
-        // final int REG_WEEK_HOURS=40;
+        final int REG_WEEK_HOURS=40;
 
         Scanner keyboard = new Scanner(System.in);
 
@@ -69,10 +69,11 @@ public class AcmePay {
         //HOURS
         System.out.println("Please enter hours worked (can be a fraction) >> ");
         hoursWorked = keyboard.nextInt();   
-        totalRegWeekPay = hoursWorked * payRate;
         if (hoursWorked >= 40) {
+            totalRegWeekPay = REG_WEEK_HOURS*payRate;
             totalOTWeekPay = (payRate*OT_RATE)*(hoursWorked-40);
         } else {
+            totalRegWeekPay = payRate * hoursWorked;
             totalOTWeekPay = 0;
         }
         //RETIREMENT PLAN
