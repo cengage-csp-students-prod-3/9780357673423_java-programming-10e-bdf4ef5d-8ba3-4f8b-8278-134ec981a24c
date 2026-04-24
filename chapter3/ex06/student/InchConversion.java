e42b788eebd5ca21dd9fd00aa1a2a4a0d8f2a8e5
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-04-24 17:23:59 +0000

    chapter3/ex06 All Tasks

--- chapter3/ex06/student/InchConversion.java
+++ chapter3/ex06/student/InchConversion.java
@@ -1,1 +1,32 @@
 // Write your code here
+import java.util.Scanner;
+
+public class InchConversion {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double inches;
+
+        // Prompt user for input
+        System.out.print("Enter inches >> ");
+        inches = input.nextDouble();
+
+        // Pass value to conversion methods
+        convertToFeet(inches);
+        convertToYards(inches);
+    }
+
+    // Method to convert inches to feet (12 inches = 1 foot)
+    public static void convertToFeet(double inches) {
+        final int INCHES_IN_FOOT = 12;
+        double feet = inches / INCHES_IN_FOOT;
+        System.out.println(inches + " inches is " + feet + " feet");
+    }
+
+    // Method to convert inches to yards (36 inches = 1 yard)
+    public static void convertToYards(double inches) {
+        final int INCHES_IN_YARD = 36;
+        double yards = inches / INCHES_IN_YARD;
+        System.out.println(inches + " inches is " + yards + " yards");
+    }
+}
