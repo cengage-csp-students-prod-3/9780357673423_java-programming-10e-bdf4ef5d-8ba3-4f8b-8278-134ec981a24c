dc4ac5b326881a605199cfa1699a3a02ef69dc88
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-04-24 17:07:49 +0000

    chapter3/ex04 All Tasks

--- chapter3/ex04/student/Percentages.java
+++ chapter3/ex04/student/Percentages.java
@@ -1,1 +1,25 @@
-// Write your code here
+import java.util.Scanner;
+
+public class Percentages {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double num1, num2;
+
+        // Prompt user for values
+        System.out.print("Enter a double >> ");
+        num1 = input.nextDouble();
+        System.out.print("Enter another double >> ");
+        num2 = input.nextDouble();
+
+        // First call: num1 as percentage of num2
+        computePercent(num1, num2);
+
+        // Second call: num2 as percentage of num1
+        computePercent(num2, num1);
+    }
+
+    public static void computePercent(double x, double y) {
+        double percentage = (x / y) * 100;
+        System.out.println(x + " is " + percentage + " percent of " + y);
+    }
+}
