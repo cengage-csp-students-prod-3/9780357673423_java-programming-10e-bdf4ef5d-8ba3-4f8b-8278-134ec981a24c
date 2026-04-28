219ff6100b1f621ac6758cedbd2dcc429ad879fd
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-04-28 21:21:11 +0000

    chapter4/ex01 All Tasks

--- chapter4/ex01/student/Sandwich.java
+++ chapter4/ex01/student/Sandwich.java
@@ -1,1 +1,31 @@
-// Write your code here
+public class Sandwich {
+    private String mainIngredient;
+    private String bread;
+    private double price;
+
+    // Setters
+    public void setMainIngredient(String ingredient) {
+        this.mainIngredient = ingredient;
+    }
+
+    public void setBread(String breadType) {
+        this.bread = breadType;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    // Getters
+    public String getMainIngredient() {
+        return mainIngredient;
+    }
+
+    public String getBread() {
+        return bread;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
