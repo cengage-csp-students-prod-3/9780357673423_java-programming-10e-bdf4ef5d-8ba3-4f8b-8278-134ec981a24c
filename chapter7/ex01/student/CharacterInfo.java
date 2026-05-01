5d7ba737d80a9fd8bed075f46245422172c6f6d4
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-05-01 17:52:05 +0000

    chapter7/ex01 All Tasks

--- chapter7/ex01/student/CharacterInfo.java
+++ chapter7/ex01/student/CharacterInfo.java
@@ -1,30 +1,39 @@
 import java.util.Scanner;
 
-public class CharacterInfo {
+public class InputCharacterInfo {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
         
-        char aChar = 'C';
+        System.out.print("Enter a character... ");
+        char aChar = scnr.next().charAt(0);
+
         System.out.println("The character is " + aChar);
-        if(Character.isUpperCase(aChar))
+        
+        if (Character.isUpperCase(aChar)) {
             System.out.println(aChar + " is uppercase");
-        else
+        } else {
             System.out.println(aChar + " is not uppercase");
-        if(Character.isLowerCase(aChar))
+        }
+
+        if (Character.isLowerCase(aChar)) {
             System.out.println(aChar + " is lowercase");
-        else
+        } else {
             System.out.println(aChar + " is not lowercase");
-        aChar = Character.toLowerCase(aChar);
-        System.out.println("After toLowerCase(), aChar is " + aChar);
-        aChar = Character.toUpperCase(aChar);
-        System.out.println("After toUpperCase(), aChar is " + aChar);
-        if(Character.isLetterOrDigit(aChar))
+        }
+
+        System.out.println("After toLowerCase(), aChar is " + Character.toLowerCase(aChar));
+        System.out.println("After toUpperCase(), aChar is " + Character.toUpperCase(aChar));
+
+        if (Character.isLetterOrDigit(aChar)) {
             System.out.println(aChar + " is a letter or digit");
-        else
-            System.out.println(aChar +
-            " is neither a letter nor a digit");
-        if(Character.isWhitespace(aChar))
+        } else {
+            System.out.println(aChar + " is neither a letter nor a digit");
+        }
+
+        if (Character.isWhitespace(aChar)) {
             System.out.println(aChar + " is whitespace");
-        else
+        } else {
             System.out.println(aChar + " is not whitespace");
+        }
     }
 }
