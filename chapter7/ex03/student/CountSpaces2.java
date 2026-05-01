3abf96e14838484854f51dd0d0504d6a394e7a6c
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-05-01 18:03:58 +0000

    chapter7/ex03 All Tasks

--- chapter7/ex03/student/CountSpaces2.java
+++ chapter7/ex03/student/CountSpaces2.java
@@ -1,1 +1,25 @@
 // Write your code here
+import java.util.Scanner;
+
+public class CountSpaces2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Prompt user for input
+        System.out.print("Enter an inspirational quote >> ");
+        String quote = input.nextLine();
+        
+        int spaceCount = 0;
+
+        // Count the spaces in the user-provided string
+        for (int i = 0; i < quote.length(); i++) {
+            if (quote.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+
+        System.out.println("The number of spaces is " + spaceCount);
+        
+        input.close();
+    }
+}
