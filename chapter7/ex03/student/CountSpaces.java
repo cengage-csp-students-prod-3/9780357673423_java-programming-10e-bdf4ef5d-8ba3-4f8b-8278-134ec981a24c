5e3c78df652b4951ce6f06aca80ad389c2058e24
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-05-01 18:01:54 +0000

    chapter7/ex03 All Tasks

--- chapter7/ex03/student/CountSpaces.java
+++ chapter7/ex03/student/CountSpaces.java
@@ -1,1 +1,18 @@
 // Write your code here
+public class CountSpaces {
+    public static void main(String[] args) {
+        // Your favorite inspirational quote
+        String quote = "The only way to do great work is to love what you do.";
+        int spaceCount = 0;
+
+        // Loop through the string to count spaces
+        for (int i = 0; i < quote.length(); i++) {
+            if (quote.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+
+        System.out.println("Quote: " + quote);
+        System.out.println("The number of spaces is " + spaceCount);
+    }
+}
