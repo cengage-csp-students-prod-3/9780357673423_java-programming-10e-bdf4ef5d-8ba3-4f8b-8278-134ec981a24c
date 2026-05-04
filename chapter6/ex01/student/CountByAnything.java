c6401e8431fa07311de3f62d18e7b2bf8d464538
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-05-04 20:58:26 +0000

    chapter6/ex01 All Tasks

--- chapter6/ex01/student/CountByAnything.java
+++ chapter6/ex01/student/CountByAnything.java
@@ -1,1 +1,25 @@
 // Write your code here
+import java.util.Scanner;
+
+public class CountByAnything {
+    public static void main(String[] args) {
+        final int START = 5;
+        final int STOP = 500;
+        final int ITEMS_PER_LINE = 10;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter number to count by >> ");
+        int step = input.nextInt();
+
+        int count = 0;
+        for (int i = START; i <= STOP; i += step) {
+            System.out.print(i + " ");
+            count++;
+            
+            // Start a new line after every 10 values
+            if (count % ITEMS_PER_LINE == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
--- chapter6/ex01/student/CountByFives.java
+++ chapter6/ex01/student/CountByFives.java
@@ -1,1 +1,15 @@
 // Write your code here
+public class CountByFives {
+    public static void main(String[] args) {
+        final int START = 5;
+        final int STOP = 500;
+        final int NEW_LINE_MULTIPLE = 50;
+
+        for (int i = START; i <= STOP; i += 5) {
+            System.out.print(i + " ");
+            if (i % NEW_LINE_MULTIPLE == 0) {
+                System.out.println();
+            }
+        }
+    }
+}
