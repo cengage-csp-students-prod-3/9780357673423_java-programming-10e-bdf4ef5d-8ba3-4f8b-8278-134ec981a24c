5e0b03678ff36062bd94aefffc1060536452bbf7
𝕋𝕒𝕓𝕒𝕣𝕖𝕤 <145236199+DanielTabares8@users.noreply.github.com>
2026-05-04 20:23:35 +0000

    chapter8/ex03 All Tasks

--- chapter8/ex03/student/CarCareChoice.java
+++ chapter8/ex03/student/CarCareChoice.java
@@ -1,1 +1,24 @@
 // Write your code here
+import java.util.Scanner;
+
+public class CarCareChoice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("oil change\ntire rotation\nbattery check\nbrake inspection");
+        System.out.print("Enter selection >> ");
+        String selection = input.nextLine();
+
+        int price = 0;
+        if (selection.equals("oil change")) price = 25;
+        else if (selection.equals("tire rotation")) price = 22;
+        else if (selection.equals("battery check")) price = 15;
+        else if (selection.equals("brake inspection")) price = 5;
+
+        if (price != 0) {
+            System.out.println(selection + " price is $" + price);
+        } else {
+            System.out.println("Invalid item entered.");
+        }
+    }
+}
--- chapter8/ex03/student/CarCareChoice2.java
+++ chapter8/ex03/student/CarCareChoice2.java
@@ -1,1 +1,37 @@
 // Write your code here
+import java.util.Scanner;
+
+public class CarCareChoice2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("oil change\ntire rotation\nbattery check\nbrake inspection");
+        System.out.print("Enter selection >> ");
+        String selection = input.nextLine();
+
+        // Safety check to ensure the string has at least 3 characters
+        String prefix = selection.length() >= 3 ? selection.substring(0, 3) : "";
+        int price = 0;
+        String fullServiceName = "";
+
+        if (prefix.equals("oil")) {
+            price = 25;
+            fullServiceName = "oil change";
+        } else if (prefix.equals("tir")) {
+            price = 22;
+            fullServiceName = "tire rotation";
+        } else if (prefix.equals("bat")) {
+            price = 15;
+            fullServiceName = "battery check";
+        } else if (prefix.equals("bra")) {
+            price = 5;
+            fullServiceName = "brake inspection";
+        }
+
+        if (price != 0) {
+            System.out.println(fullServiceName + " price is $" + price);
+        } else {
+            System.out.println("Invalid item entered.");
+        }
+    }
+}
